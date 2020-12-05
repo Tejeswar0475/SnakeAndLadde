f7f788424759c8f6593c9ce78b0dfbe5dc8ef171
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-05 12:46:10 +0530

    Added function for rolling a die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,10 +8,21 @@ public class SnakeAndLadder {
 	static int startPosition=0;
 	int noOfPlayers=1;
 
+	// UC2 to roll a die and generate random number
+	int rollingADie()
+	{
+		Random random = new Random();		//random object will generate random number
+		int rand = random.nextInt(7);		//generate from 0 to 6 numbers
+			if(rand == 0 )
+			{
+		    		rollingADie();
+		   	}
+	return rand;
+	}
 	public static void main(String[] arg)
 	{
-		SnakeAndLadder snakeandladder=new SnakeAndLadder();		//creating a object for class
-
+		SnakeAndLadder snakeandladder=new SnakeAndLadder();		//creating an object for class
+		int randomResult=snakeandladder.rollingADie();
 	}
 }
 

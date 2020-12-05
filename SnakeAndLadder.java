ada91dc86c5f6f67d2a8ea1da75a2b235c22526a
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-05 12:39:51 +0530

    Added code for initialize the memeber variables

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,11 +1,17 @@
 package com.java.snakeandladder;
 
-
+import java.util.*;
 
 public class SnakeAndLadder {
 
+	//UC1 use to initialize the member variables
+	static int startPosition=0;
+	int noOfPlayers=1;
+
 	public static void main(String[] arg)
 	{
+		SnakeAndLadder snakeandladder=new SnakeAndLadder();		//creating a object for class
 
 	}
 }
+
